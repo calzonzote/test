c0e5b665ba1ce398bbb8c3a42fbae7b574ff4516
Rubén Gómez Infante <rubengomezi@gmail.com>
2016-02-03 14:24:51 -0600

    Initial Commit

---
+++ pruebaGitHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebaGitHub
+//
+//  Created by Rubén Gómez on 2/3/16.
+//  Copyright © 2016 Rubén Gómez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
